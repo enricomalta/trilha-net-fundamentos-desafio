7300ebacedb786b3a93ff8904de38d2ea7750c22
Enrico Malta <enrico.malta1@gmail.com>
2024-09-12 18:02:45 -0300

    Desafio Concluido Enrico Malta

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -1,67 +1,113 @@
+using System.Text.Json;
+
 namespace DesafioFundamentos.Models
 {
     public class Estacionamento
     {
-        private decimal precoInicial = 0;
-        private decimal precoPorHora = 0;
-        private List<string> veiculos = new List<string>();
+        private decimal precoInicial;
+        private decimal precoPorHora;
+        private List<string> veiculos;
+        //############## IMPLEMENTAÇÃO ##############
+        private string caminhoArquivoJson = "veiculos.json"; // Deixar arquivo veiculos.json no path raiz
 
         public Estacionamento(decimal precoInicial, decimal precoPorHora)
         {
             this.precoInicial = precoInicial;
             this.precoPorHora = precoPorHora;
+            //############## IMPLEMENTAÇÃO ##############
+            this.veiculos = CarregarVeiculos(); // Carrega os veículos ao inicializar
         }
 
         public void AdicionarVeiculo()
         {
-            // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
-            // *IMPLEMENTE AQUI*
             Console.WriteLine("Digite a placa do veículo para estacionar:");
+
+            //############## IMPLEMENTAÇÃO ##############
+            string placa = Console.ReadLine();
+
+            if (!string.IsNullOrEmpty(placa))
+            {
+                if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
+                {
+                    Console.WriteLine("Veículo com essa placa já está estacionado.");
+                }
+                else
+                {
+                    veiculos.Add(placa);
+                    Console.WriteLine($"Veículo com placa {placa} adicionado ao estacionamento.");
+                    SalvarVeiculos(); // Salva na lista de veículos após adicionar
+                }
+            }
+            else
+            {
+                Console.WriteLine("Placa inválida. Tente novamente.");
+            }
         }
 
         public void RemoverVeiculo()
         {
             Console.WriteLine("Digite a placa do veículo para remover:");
 
-            // Pedir para o usuário digitar a placa e armazenar na variável placa
-            // *IMPLEMENTE AQUI*
-            string placa = "";
+            //############## IMPLEMENTAÇÃO ##############
+            string placa = Console.ReadLine();
 
-            // Verifica se o veículo existe
             if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
             {
                 Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
+                if (int.TryParse(Console.ReadLine(), out int horas))
+                {
+                    decimal valorTotal = precoInicial + precoPorHora * horas;
 
-                // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
-                // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
-                // *IMPLEMENTE AQUI*
-                int horas = 0;
-                decimal valorTotal = 0; 
-
-                // TODO: Remover a placa digitada da lista de veículos
-                // *IMPLEMENTE AQUI*
-
-                Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
+                    veiculos.Remove(placa);
+                    Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal:F2}");
+                    SalvarVeiculos(); // Salva a lista de veículos após remover
+                }
+                else
+                {
+                    Console.WriteLine("Entrada inválida para a quantidade de horas.");
+                }
             }
             else
             {
-                Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
+                Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente.");
             }
         }
 
         public void ListarVeiculos()
         {
-            // Verifica se há veículos no estacionamento
             if (veiculos.Any())
             {
                 Console.WriteLine("Os veículos estacionados são:");
-                // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
-                // *IMPLEMENTE AQUI*
+                foreach (var veiculo in veiculos)
+                {
+                    Console.WriteLine(veiculo);
+                }
             }
             else
             {
                 Console.WriteLine("Não há veículos estacionados.");
             }
+        }
+
+        private void SalvarVeiculos()
+        {   
+            //############## IMPLEMENTAÇÃO ##############
+            // Serializa a lista de veículos para JSON
+            var json = JsonSerializer.Serialize(veiculos);
+            File.WriteAllText(caminhoArquivoJson, json);
+        }
+
+        private List<string> CarregarVeiculos()
+        {   
+            //############## IMPLEMENTAÇÃO ##############
+            // Verifica se o arquivo JSON existe
+            if (File.Exists(caminhoArquivoJson))
+            {
+                // Lê o JSON do arquivo e desserializa para a lista de veículos
+                var json = File.ReadAllText(caminhoArquivoJson);
+                return JsonSerializer.Deserialize<List<string>>(json) ?? new List<string>();
+            }
+            return new List<string>();
         }
     }
 }
